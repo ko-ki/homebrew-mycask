944e771b2d4696de2688f7167c8f4060ea705897
Koki Ueda <mail@koki.ws>
2014-03-19 20:29:34 +0900

    Update totalspaces2.rb

--- Casks/totalspaces2.rb
+++ Casks/totalspaces2.rb
@@ -1,7 +1,7 @@
-class Totalspaces2 < Cask
+class TotalSpaces2 < Cask
   url 'http://downloads.binaryage.com/TotalSpaces2-2.0.17.zip'
   homepage 'http://totalspaces.binaryage.com/'
   version '2.0.17'
-  sha256 '37b044a2aa61527a4973345d25ac86d74a66c9ddd3a884ed80293c3d7946afb3'
+  sha256 'ce3f29dca8e575c4a43571396cca3623409f1abea7bc250b756018c48f3916ee'
   install 'TotalSpaces2.pkg'
 end
