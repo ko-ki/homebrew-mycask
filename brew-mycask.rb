fe917691c7d9fb72f804a1c6e748378dddc215b4
Koki Ueda <mail@koki.ws>
2014-03-19 10:28:22 +0900

    add brew-mycask.rb

---
+++ brew-mycask.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewMycask < Formula
+  homepage 'https://github.com/ko-ki/homebrew-mycask/'
+  url 'https://github.com/ko-ki/homebrew-mycask.git'
+  def install
+  end
+end
