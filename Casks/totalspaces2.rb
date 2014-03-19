4bd8edbd0720316a6d014329b95c64e37c8f8be5
Koki Ueda <mail@koki.ws>
2014-03-19 10:29:55 +0900

    add Casks/totalspaces2.rb

---
+++ Casks/totalspaces2.rb
@@ -0,0 +1,7 @@
+class TotalSpaces2 < Cask
+  url 'http://downloads.binaryage.com/TotalSpaces2-2.0.17.zip'
+  homepage 'http://totalspaces.binaryage.com/'
+  version '2.0.17'
+  sha1 '154b8e41d3b160b0160a3e4ad457e26a381acf4a'
+  install 'TotalSpaces2.pkg'
+end
